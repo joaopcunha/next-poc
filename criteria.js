206cd75906757b973d15793c60800c3407e9b397
joaopcunha <johnny.cmr@gmail.com>
2024-01-25 20:54:33 -0300

    initial commit

---
+++ criteria.js
@@ -0,0 +1,36 @@
+export default ratingCategories = [
+    {
+        slug: 'infrastructure',
+        name: 'Infraestrutura',
+        note: 'Meio porca',
+        criteria: [
+            {
+                slug: 'elevators',
+                name: 'Elevadores',
+                rate: 0
+            },
+            {
+                slug: 'hidraulics',
+                name: 'Rede Hidráulica',
+                rate: 0
+            }
+        ],
+    },
+    {   
+        slug: 'security',
+        name: 'Segurança',
+        note: '',
+        criteria: [
+            {
+                slug: 'camera',
+                name: 'Câmeras',
+                rate: 4
+            },
+            {
+                slug: 'gates',
+                name: 'Portões',
+                rate: 5
+            }
+        ]
+    }
+]
---
+++ review.js
@@ -0,0 +1,43 @@
+{
+    id: "123",
+    buildingId: "321",
+    userId: ""
+    review: [
+        {
+            slug: 'infrastructure',
+            name: 'Infraestrutura',
+            note: 'Meio porca',
+            criteria: [
+                {
+                    slug: 'elevators',
+                    name: 'Elevadores',
+                    rate: 0
+                },
+                {
+                    slug: 'hidraulics',
+                    name: 'Rede Hidráulica',
+                    rate: 0
+                }
+            ],
+        },
+        {   
+            slug: 'security',
+            name: 'Segurança',
+            note: '',
+            criteria: [
+                {
+                    slug: 'camera',
+                    name: 'Câmeras',
+                    rate: 4
+                },
+                {
+                    slug: 'gates',
+                    name: 'Portões',
+                    rate: 5
+                }
+            ]
+        }
+    ],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+}
